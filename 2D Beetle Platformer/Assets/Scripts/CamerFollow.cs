7c9caa0cb88200d8ad6d6b936f8d40d58ffe64f8
Atym-git <chuvachelaad@gmail.com>
2024-10-07 11:17:15 +0300

    Hitboxes are Tough

--- 2D Beetle Platformer/Assets/Scripts/CamerFollow.cs
+++ 2D Beetle Platformer/Assets/Scripts/CamerFollow.cs
@@ -7,11 +7,22 @@ public class CamerFollow : MonoBehaviour
     [SerializeField] float _followSpeed;
     [SerializeField] float _cameraHeight = 1f;
     [SerializeField] Transform _player;
+    [SerializeField] Camera _camera;
 
     void Update()
     {
         Vector3 newPos = new Vector3(_player.position.x, _player.position.y + _cameraHeight, -10f);
 
         transform.position = Vector3.Slerp(transform.position, newPos, _followSpeed * Time.deltaTime);
+
+        float _valueOfWheel = Input.GetAxis("Mouse ScrollWheel");
+        if (_valueOfWheel > 0)
+        {
+            _camera.orthographicSize = _valueOfWheel * Time.deltaTime;
+        }
+        else if (_valueOfWheel < 0)
+        {
+            _camera.orthographicSize = -_valueOfWheel * Time.deltaTime;
+        }
     }
 }
